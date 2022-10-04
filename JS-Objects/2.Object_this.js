2451d6ffc53b11010e66aa493170076a3fdd1593
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-10-04 23:31:10 +0600

    This is key under Nested function

---
+++ JS-Objects/2.Object_this.js
@@ -0,0 +1,12 @@
+let myCustomObj = {
+    name: "Imdadul Haque",
+    age:24,
+    job:"Software Engineer",
+    anotherObj:{
+        name:"Israt Jahan Maisha",
+        msg: function(){
+            // console.log(this);
+            console.log(this.name)
+        }
+    }
+}
